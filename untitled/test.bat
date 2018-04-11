9a747f4eb817b29359b95cc8f58653a0162884fc
SJ Park <park1215@gmail.com>
2018-04-11 14:34:20 -0600

    Add files via upload

---
+++ untitled/test.bat
@@ -0,0 +1,13 @@
+set Pathname="C:\Users\spark\PycharmProjects\untitled\seleniumScripts"
+
+cd /d %Pathname%
+
+echo Executing NewConnect_Provisioning_Prosperist.py...
+
+python.exe NewConnect_Provisioning_Prosperist.py
+
+echo Executing NewConnect_Provisioning_wb_direct.py
+
+python.exe NewConnect_Provisioning_wb_direct.py
+
+pause
